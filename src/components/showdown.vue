10ae814674dc248653d0087a7cc6186fa0ac879c
ymf961016 <1161015103@qq.com>
2021-02-20 18:17:37 +0800

    表格支持

--- src/components/showdown.vue
+++ src/components/showdown.vue
@@ -1,7 +1,6 @@
 // md语法展示
 <template>
-  <div class="code-wrap" v-html="html" v-highlight>
-  </div>
+  <div class="code-wrap" v-html="html" v-highlight></div>
 </template>
 <script>
 export default {
@@ -17,16 +16,35 @@ export default {
       html: '',
     }
   },
-  watch:{
-    text(){
-      let converter = new this.showdown.Converter()
+  watch: {
+    text() {
+      let converter = new this.showdown.Converter({ tables: true })
       this.html = converter.makeHtml(this.text)
-    }
+    },
   },
 }
 </script>
 <style lang="less" scoped>
-  .code-wrap{
-    font-size: 16px;
+.code-wrap {
+  font-size: 16px;
+  /deep/table {
+    border-collapse: collapse;
+    margin-bottom: 10px;
+    margin-top: 10px;
+      text-align: center;
+    tr {
+      background-color: #fff;
+      border-top: 1px solid #c6cbd1;
+    }
+    th {
+      padding: 6px 13px;
+      border: 1px solid #dfe2e5;
+      font-weight: 600;
+    }
+    td {
+      padding: 6px 13px;
+      border: 1px solid #dfe2e5;
+    }
   }
+}
 </style>
